9f4e4b605937465b39d9fa3c1159b52a4c320437
yuryzinchuk <50735999+yuryzinchuk@users.noreply.github.com>
2019-05-17 12:57:47 +0300

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -41,54 +41,54 @@ void ShowQueue(ListType list) {
 int main()
 {
     int n;
-	int m;
-	int i=0;
-	int SIZEQ; // Queue size
-	int TRANS_SLOW=3; // transmit each 3th message
+    int m;
+    int i=0;
+    int SIZEQ; // Queue size
+    int TRANS_SLOW=3; // transmit each 3th message
     Node *head=0;
 
-	std::cout << "Queue size: "; std::cin >> SIZEQ;
+    std::cout << "Queue size: "; std::cin >> SIZEQ;
     std::cout << std::endl;
 
     while (std::cout << "add new: ", std::cin >> n)
     {
-	 // insert a new message in the head of the list (to the queue)
+     // insert a new message in the head of the list (to the queue)
      Node *p=new Node;
      p->prior=n;
      p->next=head;
      head=p;
-	 // insertion sort
+     // insertion sort
      InsertAndSort(head);
-	 // show sorted queue
-	 m=0;
-	 p=head;
+     // show sorted queue
+     m=0;
+     p=head;
      while (p!=NULL)
-	 {
+     {
       std::cout << p->prior << " ";
-	  m++;
-	  if (p->next != NULL)
-	  {
+      m++;
+      if (p->next != NULL)
+      {
        // control of size 
-	   if (m==SIZEQ) 
-	   {
+       if (m==SIZEQ) 
+       {
         // delete a message with low priority
         std::cout << "deleted with low priority: " << p->next->prior;
-	    delete p->next;
-		p->next = NULL;
-	  };
-	 };
-	 p=p->next;
-	}
-	// transmit a message with high priority
-	if (++i >= TRANS_SLOW)
-	{
-     i=0;
- 	 p=head;
-	 head=head->next;
-     std::cout << std::endl << "transmit with high priority: " << p->prior;
-	 delete p;
-	 ShowQueue(head);
-	 };
+        delete p->next;
+        p->next = NULL;
+       };
+      };
+      p=p->next;
+     }
+     // transmit a message with high priority
+     if (++i >= TRANS_SLOW)
+     {
+      i=0;
+      p=head;
+      head=head->next;
+      std::cout << std::endl << "transmit with high priority: " << p->prior;
+      delete p;
+      ShowQueue(head);
+     };
      std::cout << std::endl;
     }
 }
